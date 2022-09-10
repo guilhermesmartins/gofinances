c12370679ed74215d6c567a6688da4b9f9688c8b
Guilherme Martins <guilhermesmartins@Guilhermes-MacBook-Pro.local>
2022-09-10 13:23:11 -0300

    feat: setup add

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Welcome from "./src/Components/Welcome";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Welcome title="oi" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
